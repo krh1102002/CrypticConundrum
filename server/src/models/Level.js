d07d931bdc0fc0458aa79fb3f86cc44f0dba04fa
Anas5006 <anassain13@gmail.com>
2023-05-07 22:04:48 +0530

    Rmoved Bcrypt error

--- server/src/models/Level.js
+++ server/src/models/Level.js
@@ -6,13 +6,5 @@ const levelSchema = new mongoose.Schema({
     image:{type:String,required:true}
 })
 
-levelSchema.pre("save", async function (next) {
-    if (!this.isModified) {
-      next();
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    this.word = await bcrypt.hash(this.word, salt);
-});
 
 module.exports = mongoose.model('levels',levelSchema)
